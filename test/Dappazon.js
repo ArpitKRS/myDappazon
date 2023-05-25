22daa87663f193b014a9568186ef6a7255e5e0e9
Arpit Kumar Singh <singharpit302@gmail.com>
2023-05-26 01:00:32 +0530

    contract layout

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -6,4 +6,21 @@ const tokens = (n) => {
 
 describe("Dappazon", () => {
 
+  let dappazon
+  let deployer, buyer
+
+  beforeEach(async () => {
+    // Setup Accounts
+    [deployer, buyer] = await ethers.getSigners()
+
+    // Deploy contract
+    const Dappazon = await ethers.getContractFactory('Dappazon')
+    dappazon = await Dappazon.deploy()
+  })
+
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+  })
 })
